a4b41a63e2e40dcf3ee1b4ab93ec7a3360d4acbe
light-200 <44830619+light-200@users.noreply.github.com>
2022-08-31 13:46:17 +0530

    first commit ☄️

---
+++ src/containers/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>nav</p>
+      <section>banner</section>
+      <section>
+        <h3>nav2</h3>
+        <p>section2</p>
+      </section>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
