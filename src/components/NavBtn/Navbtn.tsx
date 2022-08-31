003eceee23fbc89b70991d68de96a4c4ce39fdb8
light-200 <44830619+light-200@users.noreply.github.com>
2022-09-01 01:15:52 +0530

    fix: btn hover and active btn fixed 🚀

--- src/components/NavBtn/Navbtn.tsx
+++ src/components/NavBtn/Navbtn.tsx
@@ -1,15 +1,13 @@
 import classes from "./Navbtn.module.css";
-import { useState } from "react";
 
 interface Prop {
   name: string;
   handleClick: Function;
   children: any;
+  active?: Boolean;
 }
 
-const Navbtn = ({ children, name, handleClick }: Prop) => {
-  const [active, setActive] = useState(false);
-
+const Navbtn = ({ children, name, handleClick, active }: Prop) => {
   let bntClasses = active
     ? `${classes.Navbtn} ${classes.Active}`
     : classes.Navbtn;
@@ -19,16 +17,6 @@ const Navbtn = ({ children, name, handleClick }: Prop) => {
       className={bntClasses}
       onClick={() => {
         handleClick(name);
-      }}
-      onMouseOver={() => {
-        setActive((active) => {
-          return !active;
-        });
-      }}
-      onMouseLeave={() => {
-        setActive((active) => {
-          return !active;
-        });
       }}
     >
       {children}
