d030325bb367464dea793cfed9712d1d5b7bf460
light-200 <44830619+light-200@users.noreply.github.com>
2022-08-31 20:51:39 +0530

    feat: added css and components 🚀

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import Banner from "../components/Banner/Banner";
+import Navbtn from "../components/NavBtn/Navbtn";
+import Section from "../components/Section/Section";
+import Logo from "../assets/logo.png";
 import "./App.css";
 
 function App() {
+  const [section, setSection] = useState("event");
+  const handleClick = (name: string) => {
+    setSection(name);
+  };
+
   return (
     <div className="App">
-      <p>nav</p>
-      <section>banner</section>
-      <section>
-        <h3>nav2</h3>
-        <p>section2</p>
+      <div className="nav">
+        <div className="logo">
+          <img src={Logo} />
+        </div>
+        <div className="title">
+          Govind Ballabh Pant Institue of engineering and technology
+          <p>An autonomous institue of Government of Uttarakhand</p>
+        </div>
+        <div className="socials"></div>
+      </div>
+      <Banner />
+      <section className="Main">
+        <div className="nav2">
+          <Navbtn handleClick={handleClick} name={"event"}>
+            Events
+          </Navbtn>
+          <Navbtn handleClick={handleClick} name={"student"}>
+            Student Coordinators
+          </Navbtn>
+          <Navbtn handleClick={handleClick} name={"faculty"}>
+            Faculty Coordinators
+          </Navbtn>
+          <Navbtn handleClick={handleClick} name={"clubs"}>
+            Clubs
+          </Navbtn>
+        </div>
+        <Section id={section} />
       </section>
       <footer>footer</footer>
     </div>
