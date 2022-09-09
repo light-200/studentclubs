023b875c6e6ea99948ba1847dd47d1d631f2a449
light-200 <44830619+light-200@users.noreply.github.com>
2022-09-09 18:38:39 +0530

    feat: added router

---
+++ src/containers/EventPage/EventPage.jsx
@@ -0,0 +1,10 @@
+import classes from "./EventPage.module.css";
+const EventPage = () => {
+  return (
+    <div className={classes.EventPage}>
+      <h1></h1>
+      <img />
+      <p></p>
+    </div>
+  );
+};
